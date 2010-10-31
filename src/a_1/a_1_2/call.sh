08ebd29ad1e470d2c60b602934e82398342c0dd8
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-10-31 19:54:23 +0100

    kommentare und mehrfache ausführung

--- src/a_1/a_1_2/call.sh
+++ src/a_1/a_1_2/call.sh
@@ -3,21 +3,45 @@
 # call summation with different arguments
 #
 
+####
 # abort on Ctrl+C
+####
 trap 'echo Abort; exit 1' 2
 
+call5Times () {
+	for i in $(seq 1 5)
+	do
+		eval	$1
+	done
+}
+
+
+####
+# calculation
+####
+
+echo "============ PHASE 1 ==========="
 for i in $(seq 1 8)
 do
 	# number of processors
 	NUM=10000
-	echo  Using $i PEs to sum up $NUM elements
-	time mpirun -np $i ./summation_mpi $NUM 1
+	echo  Using $i PEs to sum up $NUM elements 5 times
+	# env: call /usr/bin/time instead of shell built-in
+	# we try to reproduce each call 5 times to get some useable stats.
+	time for j in $(seq 1 5)
+	do
+		mpirun -np $i ./summation_mpi $NUM 0
+	done
 	echo ""
 done
 
+echo "============ PHASE 2 ==========="
 for i in 1000 10000 10000 100000 1000000 
 do
-	echo  Using 4 PEs to sum up $i elements
-	time mpirun -np 4 ./summation_mpi $i 1
+	echo  Using 4 PEs to sum up $i elements 5 times
+	time for j in $(seq 1 5)
+	do
+		mpirun -np 4 ./summation_mpi $i 0
+	done
 	echo ""
 done
