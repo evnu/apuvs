2d72df24ca07f3400a79f431d95b1baba3799b8a
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-04 12:46:04 +0100

    added default send method to call

--- src/a_2/a_2_1/call.sh
+++ src/a_2/a_2_1/call.sh
@@ -25,12 +25,13 @@ for i in $(seq 1 8)
 do
 	# number of processors
 	NUM=10000
-	echo  Using $i PEs to sum up $NUM elements 5 times
+	s=d
+	echo  Using $i PEs to sum up $NUM elements 5 times with method $s
 	# env: call /usr/bin/time instead of shell built-in
 	# we try to reproduce each call 5 times to get some useable stats.
 	time for j in $(seq 1 5)
 	do
-		mpirun -np $i ./summation_mpi $NUM 0
+		mpirun -np $i ./summation_mpi $NUM $s 0
 	done
 	echo ""
 done
@@ -39,9 +40,10 @@ echo "============ PHASE 2 ==========="
 for i in 1000 10000 10000 100000 1000000 
 do
 	echo  Using 1 PEs to sum up $i elements 5 times
+	s=d
 	time for j in $(seq 1 5)
 	do
-		mpirun -np 1 ./summation_mpi $i 0
+		mpirun -np 1 ./summation_mpi $i $s 0
 	done
 	echo ""
 done
@@ -50,9 +52,10 @@ echo "============ PHASE 3 ==========="
 for i in 1000 10000 10000 100000 1000000 
 do
 	echo  Using 4 PEs to sum up $i elements 5 times
+	s=d
 	time for j in $(seq 1 5)
 	do
-		mpirun -np 4 ./summation_mpi $i 0
+		mpirun -np 4 ./summation_mpi $i $s 0
 	done
 	echo ""
 done
