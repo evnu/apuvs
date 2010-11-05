cdfc4c428be5bf313acd98d744689edcf271ec22
Jan <jangingnicht@web.de>
2010-11-05 17:23:33 +0100

    callscript

---
+++ src/a_2/a_2_3/call.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# call summation with different arguments
+#
+
+####
+# abort on Ctrl+C
+####
+trap 'echo Abort; exit 1' 2
+
+call5Times () {
+	for i in $(seq 1 5)
+	do
+		mpirun -np 3 receive_mpi
+        echo ""
+	done
+}
+
+
+####
+# calculation
+####
+
+echo "============ Calling receive 5 times ==========="
+call5Times
