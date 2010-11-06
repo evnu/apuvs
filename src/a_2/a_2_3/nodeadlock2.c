83911c4d12245a3578cd23d59a4b060e50e1a410
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-06 14:05:34 +0100

    nodeadlock2 works

--- src/a_2/a_2_3/nodeadlock2.c
+++ src/a_2/a_2_3/nodeadlock2.c
@@ -22,8 +22,8 @@ int main (int argc, char **argv) {
 	int myid;
 	MPI_Comm_rank (MPI_COMM_WORLD, &myid);
 
-	// Avoiding deadlock by using MPI_Sendrecv_replace - Sends and receives a message using
-	// a single buffer
+	// deadlock avoidance: PE 0 sends and recieves using the same function call, PE 1 uses
+	// its own buffer to avoid blocking on send.
 	if (myid == 0) {
 		// send message to 1, wait for message from 1
 		char buf[10000];
@@ -34,11 +34,15 @@ int main (int argc, char **argv) {
 	} else {
 		// send message to 0, wait for message from 0
 		char buf[10000];
+		char intermediate_buffer[10000 + MPI_BSEND_OVERHEAD];
+		MPI_Buffer_attach (&intermediate_buffer, 10000 + MPI_BSEND_OVERHEAD);
+
 		MPI_Status stat;
-		MPI_Sendrecv_replace (buf, 10000, MPI_CHAR, 1, 0, 1, 0,MPI_COMM_WORLD, &stat);
+		MPI_Bsend (buf, 10000, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+		// we can use buf again, as intermediate_buffer will take care of buffering
+		MPI_Recv (buf, 10000, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &stat);
 		printf ("1: done\n");
 	}
-	printf ("done\n");
 	MPI_Finalize ();
 
 	return EXIT_SUCCESS;
