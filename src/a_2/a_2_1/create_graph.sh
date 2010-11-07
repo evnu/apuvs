0a5f37b9eb8c9ef6f61b5ecbaa9cdab974709252
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-07 16:03:51 +0100

    graphs

---
+++ src/a_2/a_2_1/create_graph.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# create graphs from data in data/ using gnuplot template gnuplot.gpi
+#
+# Requirements:
+#		* gnuplot
+#   * m4
+
+
+m4 -DDISTRIBUTED="fixedarray_1000000_blockingsendtype_distributed" \
+	 -DSINGLEMACHINE="fixedarray_1000000_blockingsendtype_singlemachine" \
+	 -DTITLE="Blocking Send" \
+	 -DFILENAME="blockingsend" gnuplot.gpi | gnuplot
+
+m4 -DDISTRIBUTED="fixedarray_1000000_defaultsendtype_distributed" \
+	 -DSINGLEMACHINE="fixedarray_1000000_defaultsendtype_singlemachine" \
+	 -DTITLE="Default Send" \
+	 -DFILENAME="defaultsendtype" gnuplot.gpi | gnuplot
+
+m4 -DDISTRIBUTED="fixedarray_1000000_readysendtype_distributed" \
+	 -DSINGLEMACHINE="fixedarray_1000000_readysendtype_singlemachine" \
+	 -DTITLE="Ready Send" \
+	 -DFILENAME="readysendtype" gnuplot.gpi | gnuplot
+
+m4 -DDISTRIBUTED="fixedarray_1000000_synchronsendtype_distributed" \
+	 -DSINGLEMACHINE="fixedarray_1000000_synchronsendtype_singlemachine" \
+	 -DTITLE="Synchronous Send" \
+	 -DFILENAME="synchronsendtype" gnuplot.gpi | gnuplot
