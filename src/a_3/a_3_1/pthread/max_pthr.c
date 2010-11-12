44c7dc81b9a6f0fbd82fce532b74c156c17552ab
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-12 19:53:00 +0100

    a lot of splint corrections

--- src/a_3/a_3_1/pthread/max_pthr.c
+++ src/a_3/a_3_1/pthread/max_pthr.c
@@ -8,45 +8,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <time.h>
 #include <sys/time.h> 
 
 #include "messuretime.h"
 
-struct timeval begin, end;
-double delta;
-int max_thr = 10000;
+// we only need compilation unit scope for this variables
+static struct timeval begin, end;
+static double delta;
+static int max_thr = 10000;
 
 // mutex to avoid race conditions
 // the time needed to acquire the mutex isn't messured
 pthread_mutex_t taketime = PTHREAD_MUTEX_INITIALIZER;
 
 // the thread function
-void* go(void* i);
+static void* go(void* i);
 
 int main (int argc, char **argv){
-	if (argc > 1) {
-		sscanf (argv[1], "%d", &max_thr);
-	}
 	long i = 1;
+	if (argc > 1) {
+		(void) sscanf (argv[1], "%d", &max_thr);
+	}
+
 	// just call the recursive function
-	go ((void *) (&i));
+	(void) go ((void *) (&i)); // cast to avoid splint warning message
 
 	exit(EXIT_SUCCESS);
 }
 
 void *go (void *i) {
 	long threadnr = * ((long*)(i));
+	int pthread_error_number;
+	pthread_t th;
+
 	if(threadnr < max_thr){
-		pthread_t th;
-		pthread_mutex_lock (&taketime);
-			threadnr++;
-			gettimeofday (&begin, NULL);
-				pthread_create(&th,NULL,go, (void *)(&threadnr));
-			gettimeofday (&end, NULL);
-			delta+=mdiff (&begin, &end);
-		pthread_mutex_unlock (&taketime);
-		pthread_join(th,NULL);
+		if ((pthread_error_number = pthread_mutex_lock (&taketime))) {
+			printf ("I couldn't acquire a lock in line %d due to error number %d\n",
+					__LINE__, pthread_error_number);
+			exit (EXIT_FAILURE);
+		}
+		threadnr++;
+		gettimeofday (&begin, NULL);
+		if ((pthread_error_number = pthread_create(&th, 
+																	NULL /*@null@*/,
+																	go, (void *)(&threadnr)
+																	)
+			 ))
+		{
+			printf ("Aborting due to errno = %d\n", pthread_error_number);
+			printf("CLOCKS_PER_SEC: %ld\n",CLOCKS_PER_SEC);
+			printf("Delta: %ld\n",(long)delta);
+			printf("%d Threads started %.8f Sec/Thread\n",max_thr,((float)delta/max_thr)/CLOCKS_PER_SEC);
+		}
+		gettimeofday (&end, NULL);
+		delta += mdiff (&begin, &end);
+		(void) pthread_mutex_unlock (&taketime);
+		(void) pthread_join(th,NULL);
 	}else{
 		printf("CLOCKS_PER_SEC: %ld\n",CLOCKS_PER_SEC);
 		printf("Delta: %ld\n",(long)delta);
--- src/a_3/a_3_1/pthread/messuretime.h
+++ src/a_3/a_3_1/pthread/messuretime.h
@@ -11,6 +11,6 @@ double mdiff (struct timeval *start, struct timeval *end) {
 	long seconds  = end->tv_sec  - start->tv_sec;
 	double useconds = end->tv_usec - start->tv_usec;
 
-	return ((seconds) * 1000 + useconds/1000.0) + 0.5;
+	return (((seconds) * 1000 + useconds/1000.0) + 0.5);
 }
 
