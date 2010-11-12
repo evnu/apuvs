82fe87191655cc27173a4fff684bdb47792f977e
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-12 19:17:59 +0100

    i'll restructure the pthread implementation

---
+++ src/a_3/a_3_1/pthread/max_pthr.c
@@ -0,0 +1,54 @@
+/*
+ * =====================================================================================
+ *       Filename:  max_pthr.c
+ *    Description:  Produce Posix-Threads and measure the time to create them
+ *        Created:  09.11.2010 20:43:23
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h> 
+
+clock_t begin, end, delta;
+int max_thr = 10000;
+
+void* go(void* i){
+	pthread_t th;
+	long threadnr = (long)i;
+	if(threadnr < max_thr){
+		threadnr++;
+		// TODO WTF! Asking for raceconditions, are we?
+		begin=clock();
+		pthread_create(&th,NULL,go, (void *)threadnr);
+		end=clock();
+		delta+=(end-begin);
+		pthread_join(th,NULL);
+	}else{
+		printf("CLOCKS_PER_SEC: %ld\n",CLOCKS_PER_SEC);
+		printf("Delta: %ld\n",(long)delta);
+		printf("%d Threads started %.8f Sec/Thread\n",max_thr,((float)delta/max_thr)/CLOCKS_PER_SEC);
+	}
+	return NULL; /* we have to return something.. NULL is better than some random value */
+}
+
+
+int main (int argc, char **argv){
+	if (argc > 1) {
+		sscanf (argv[1], "%d", &max_thr);
+	}
+
+	long i=1;
+	pthread_t thr;
+	begin=clock();
+	if(pthread_create(&thr,NULL,go,(void *)i) >= 0){
+		end=clock();
+		delta=end-begin;
+		pthread_join(thr,NULL);
+	}
+	else
+		perror("pthread_create failed: ");
+	exit(0);
+
+}
