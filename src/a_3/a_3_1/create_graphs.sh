d04dccc52ebcf1ac14f74fc36d05de703cf5aa9d
Kai Warncke <warncke@informatik.hu-berlin.de>
2010-11-15 15:10:33 +0100

    create_graphs.sh edited pthread section deleted

--- src/a_3/a_3_1/create_graphs.sh
+++ src/a_3/a_3_1/create_graphs.sh
@@ -6,6 +6,5 @@
 m4 -DTITLE="Fork" -DFILENAME="fork" -DINPUT="data/fork.dat" gnuplot.gpi | gnuplot
 m4 -DTITLE="Fork and Exec" -DFILENAME="fork_exec" -DINPUT="data/fork_exec.dat" gnuplot.gpi |gnuplot
 m4 -DTITLE="Fork with working childs" -DFILENAME="fork_childsworking" -DINPUT="data/fork_childsworking.dat" gnuplot.gpi |gnuplot
-m4 -DTITLE="pthread with sleeping threads" -DFILENAME="pthread" -DINPUT="data/pthread.dat" gnuplot.gpi |gnuplot
 m4 -DTITLE="max_pthr" -DFILENAME="max_pthr" -DINPUT="data/max_pthr.dat" gnuplot.gpi | gnuplot
 
