649026bea94dd4eb133d9566343698cd43dcff08
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-18 16:36:41 +0100

    initial create_graphs and gnuplot.gpi

---
+++ src/a_4/a_4_1/create_graphs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# some m4 calls
+#
+#
+m4 -DTITLE="Fork" -DFILENAME="fork" -DINPUT="data/fork.dat" gnuplot.gpi | gnuplot
+m4 -DTITLE="Fork and Exec" -DFILENAME="fork_exec" -DINPUT="data/fork_exec.dat" gnuplot.gpi |gnuplot
+m4 -DTITLE="Fork with working childs" -DFILENAME="fork_childsworking" -DINPUT="data/fork_childsworking.dat" gnuplot.gpi |gnuplot
+m4 -DTITLE="max_pthr" -DFILENAME="max_pthr" -DINPUT="data/max_pthr.dat" gnuplot.gpi | gnuplot
+m4 -DTITLE="Java Threads" -DFILENAME="javathreads" -DINPUT="data/java.dat" gnuplot.gpi | gnuplot
+
