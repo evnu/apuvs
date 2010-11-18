720e81ec4277bc680b24797709f3f74674d8d8c4
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-18 16:33:55 +0100

    use other paramaters

--- src/a_4/a_4_1/call.sh
+++ src/a_4/a_4_1/call.sh
@@ -8,7 +8,12 @@
 ##### global variables
 DATADIR=data/
 
-arraysize=$(seq 10 10 90; seq 100 100 900; seq 1000 1000 90000; seq 100000 100000 1000000)
+
+
+################ summation.c
+echo "Calling summation"
+
+arraysize=$(seq 10 10 90; seq 100 100 900; seq 1000 1000 9000; seq 10000 10000 100000)
 
 for num_threads in $(seq 1 10)
 do
@@ -25,3 +30,23 @@ do
 	done
 done
 
+################ summation_ascending_nums.c
+
+echo "Calling summation_ascending_nums"
+
+arraysize=$(seq 10 10 90; seq 100 100 900; seq 1000 1000 10000)
+
+for num_threads in $(seq 1 10)
+do
+	for size in $arraysize
+		# $arraysize
+	do 
+		evaluation=$(
+		for i in $(seq 1 10)
+		do
+			./summation_ascending_nums $size $num_threads 
+		done |grep TIME| awk '{sum=sum+$2;} END {printf sum/NR;}'
+		)
+		echo $num_threads $size $evaluation >> $DATADIR/"ascending_$num_threads".dat
+	done
+done
