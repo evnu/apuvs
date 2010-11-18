7785ab0635a3e58437f38895da1da432999337cf
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-18 22:06:02 +0100

    added more evaluation

--- src/a_4/a_4_1/call.sh
+++ src/a_4/a_4_1/call.sh
@@ -9,6 +9,29 @@
 DATADIR=data/
 
 
+###### calculate median
+median() {
+  args=($( for i in $*; do echo $i; done | sort -n ))
+  mid=$(( $#/2 ))
+  if (( $#%2 == 0 )); then
+    echo "scale=2;(${args[mid]}+${args[mid-1]})/2"|bc -l
+  else
+    echo ${args[mid]}
+  fi
+}
+
+###### get smallest element
+smallest () {
+	args=($(for i in $*; do echo $i; done | sort -n))
+	echo ${args[0]}
+}
+
+###### calculate mean
+average () {
+	args=$(for i in $*; do echo $i; done | \
+		awk '{sum=sum+$1;} END {printf sum/NR;}')
+	echo $args
+}
 
 ################ summation.c
 echo "Calling summation"
@@ -21,12 +44,14 @@ do
 		# $arraysize
 	do 
 		evaluation=$(
-		for i in $(seq 1 10)
+		for i in $(seq 1 30)
 		do
 			./summation $size $num_threads 
-		done |grep TIME| awk '{sum=sum+$2;} END {printf sum/NR;}'
+		done |grep TIME | sed 's/TIME: //g'
 		)
-		echo $num_threads $size $evaluation >> $DATADIR/"summation.$num_threads".dat
+		echo $num_threads $size $(smallest $evaluation) >> $DATADIR/"summation.smallest.$num_threads".dat
+		echo $num_threads $size $(median $evaluation) >> $DATADIR/"summation.median.$num_threads".dat
+		echo $num_threads $size $(average $evaluation) >> $DATADIR/"summation.$num_threads".dat
 	done
 done
 
@@ -45,8 +70,10 @@ do
 		for i in $(seq 1 10)
 		do
 			./summation_ascending_nums $size $num_threads 
-		done |grep TIME| awk '{sum=sum+$2;} END {printf sum/NR;}'
+		done |grep TIME
 		)
-		echo $num_threads $size $evaluation >> $DATADIR/"ascending.$num_threads".dat
+		echo $num_threads $size $(smallest $evaluation) >> $DATADIR/"ascending.smallest.$num_threads".dat
+		echo $num_threads $size $(median $evaluation) >> $DATADIR/"ascending.median.$num_threads".dat
+		echo $num_threads $size $(average $evaluation) >> $DATADIR/"ascending.$num_threads".dat
 	done
 done
