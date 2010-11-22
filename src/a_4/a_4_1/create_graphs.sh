cb0bd28297336b5b9ee1593ba3361aa11fc2cd2c
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2010-11-22 15:54:57 +0100

    title of graphs were wrong

--- src/a_4/a_4_1/create_graphs.sh
+++ src/a_4/a_4_1/create_graphs.sh
@@ -3,13 +3,13 @@
 # some m4 calls
 #
 #
-m4 -DTITLE="Sum up array, averaging 10 runs" -DFILENAME1="summation_1st" \
+m4 -DTITLE="Sum up array, averaging 30 runs" -DFILENAME1="summation_1st" \
 	-DFILENAME2="summation_2nd" -DFILENAME3="summation_3rd" \
 	-DFILE="summation" gnuplot.gpi | gnuplot
-m4 -DTITLE="Sum up array using median, median out of 10 runs" -DFILENAME1="summation_median_1st" \
+m4 -DTITLE="Sum up array using median, median out of 30 runs" -DFILENAME1="summation_median_1st" \
 	-DFILENAME2="summation_median_2nd" -DFILENAME3="summation_median_3rd" \
 	-DFILE="summation.median" gnuplot.gpi | gnuplot
-m4 -DTITLE="Sum up array, fastest out of 10 runs" -DFILENAME1="summation_smallest_1st" \
+m4 -DTITLE="Sum up array, fastest out of 30 runs" -DFILENAME1="summation_smallest_1st" \
 	-DFILENAME2="summation_smallest_2nd" -DFILENAME3="summation_smallest_3rd" \
 	-DFILE="summation.smallest" gnuplot.gpi | gnuplot
 
