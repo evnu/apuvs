c7f953f04539eb279588cfb5118cc4e4612ce928
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-01-11 13:21:01 +0100

    ratemal: bugfix (grouping, missed one process), output and rpc
    * rpc = wait for reply from ApplicationLayers

--- src/a_8/src/ratemal.erl
+++ src/a_8/src/ratemal.erl
@@ -9,16 +9,18 @@ initialization(Collector, Parent) ->
 
 raten(Collector, MaekawaPid) ->
     receive
-        access_critical_section ->
-            io:format("~w is asking for resources\n",[self()]),
+        {access_critical_section, Sender} ->
+            io:format("~w is asking for resources at ~w\n",[self(), MaekawaPid]),
             MaekawaPid ! {m_enter_cs, self()},
             receive
                 {a_ok, MaekawaPid} -> 
+                    io:format("~w got a_ok from ~w\n",[self(), MaekawaPid]),
                     Collector ! {c_collect, {MaekawaPid, self(),a_ok}}
             end,
             io:format ("~w is in critical section nowi\n", [self()]),
             MaekawaPid ! {m_exit_cs, self()},
-            io:format ("~w left critical section nowi\n", [self()])
+            io:format ("~w left critical section nowi\n", [self()]),
+            Sender ! {a_done, self()}
     end,
     raten(Collector, MaekawaPid).
 
@@ -31,11 +33,21 @@ creator(Anzahl) ->
     makeGroups([], Anzahl), 
 
     % test the functionality
-    Initiator3 ! access_critical_section,
-    Initiator1 ! access_critical_section,
+    Initiator3 ! {access_critical_section, self()},
+    Initiator1 ! {access_critical_section, self()},
     timer:sleep(1000),
+    % wait for reply from Initiator1 and Initiator3
+    receive 
+        {a_done, Initiator1} ->
+            io:format("~w is done\n", [Initiator1])
+    end,
+    receive 
+        {a_done, Initiator3} ->
+            io:format("~w is done\n", [Initiator3])
+    end,
+
     % tell collector to print msc
-    C ! {c_print_to_file, "../doc/msc.msc"},
+    % C ! {c_print_to_file, "../doc/msc.msc"},
     C ! {c_stop}.
 
 overlap ([], _, Acc) ->
@@ -50,7 +62,7 @@ overlap ([H|T]) ->
 partition ([], Accum, _) -> 
     %% overlap 
     overlap (Accum);
-partition ([_|PidList], [H|T], Ideal) when length(PidList) < Ideal ->
+partition (PidList, [H|T], Ideal) when length(PidList) < Ideal ->
     partition ([], [lists:append(H, PidList) | T], Ideal);
 partition (PidList, Accum, Ideal) -> 
     {NewGroup, Tail} = lists:split(Ideal, PidList),
@@ -59,6 +71,7 @@ partition (PidList, Accum, Ideal) ->
 makeGroups(PidList, 0) when length(PidList) > 0 ->
     Ideal = loor(math:sqrt(length(PidList))),
     GroupList = partition (PidList, [], Ideal),
+    io:format("grouplist = ~w\n", [GroupList]),
     [[Pid ! {m_group, Group}|| {Pid,_} <- Group]|| Group <- GroupList]
     ;
 
