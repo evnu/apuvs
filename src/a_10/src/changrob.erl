9534cd529be117acb3cec0b78e14d078ecce3ff9
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-01-20 16:55:18 +0100

    msc for exc. 10

--- src/a_10/src/changrob.erl
+++ src/a_10/src/changrob.erl
@@ -1,50 +1,58 @@
 -module(changrob).
--export([initcr/0, cr/2]).
+-export([initcr/1, cr/3]).
 
-initcr() ->
+initcr(Collector) ->
     receive
         Next ->
-            cr(Next, false)
+            cr(Next, false, Collector)
     end.
 
-cr(Next, Participant) ->
+cr(Next, Participant, Collector) ->
     receive
         {start_election, _} ->
             io:fwrite("~w: starting election\n", [self()]),
-            Next ! {election, self()},
-            cr(Next, true);
-        {election, Pred} ->
+            Collector ! {c_state_change, {self(), start_election}},
+
+            Next ! {{election, self()}, self()},
+            cr(Next, true, Collector);
+        {{election, Pred}, Sender}->
+            Collector ! {c_collect, {Sender, self(), io_lib:format("<election, ~w>", [Pred])}},
             case (le(self(), Pred)) of
                 true ->
                     io:fwrite("~w: received <election, ~w>...passing it on\n",[self(), Pred]),
-                    Next ! {election, Pred},
-                    cr(Next, true);
+
+                    Next ! {{election, Pred}, self()},
+                    cr(Next, true, Collector);
                 false ->
                     if
                         Participant ->
                             discardMsg;
                         not Participant ->
                             io:fwrite("~w: received <election, ~w>...proposing myself\n",[self(), Pred]),
-                            Next ! {election, self()}
+                            Next ! {{election, self()}, self()}
                     end,
-                    cr(Next, Participant);
+                    cr(Next, Participant, Collector);
                 equal ->
                     io:fwrite("~w: received <election, ~w>...i was elected\n",[self(), Pred]),
-                    Next ! {elected, self()},
-                    cr(Next, false)
+
+                    Next ! {{elected, self()}, self()},
+                    cr(Next, false, Collector)
             end;
-        {elected, Leader} ->
+        {{elected, Leader}, Pred} ->
             %report the leader to somewhere
             case (self() == Leader) of
                 false ->
                     io:fwrite("~w: elected ~w to be the leader\n", [self(),
                             Leader]),
-                    Next ! {elected, Leader},
-                    cr(Next, false);
+                    Collector ! {c_collect, {Pred, self(), io_lib:format("<elected, ~w>", [Leader])}},
+                    Next ! {{elected, Leader}, self()},
+                    cr(Next, false, Collector);
                 true ->
                     io:fwrite("~w: elected ~w to be the leader\n", [self(),
                             Leader]),
-                    cr(Next, false)
+                    Collector ! {c_collect, {Pred, self(), io_lib:format("<elected, ~w>", [Leader])}},
+                    Collector ! {c_state_change, {self(), leader}},
+                    cr(Next, false, Collector)
             end
     end.
 
--- src/a_10/src/test.erl
+++ src/a_10/src/test.erl
@@ -2,8 +2,19 @@
 -export([testcr/1]).
 
 testcr(N)->
-    [H|T] = Pids = [spawn(changrob, initcr, []) || _ <- lists:seq(1, N)],
+    % create a collector to build msc trace
+    C = collector:start_collector(),
+    [H|T] = Pids = [spawn(changrob, initcr, [C]) || _ <- lists:seq(1, N)],
     SendTupels = lists:zip(Pids, lists:append(T, [H])),
     lists:map(fun ({Pred, Next}) -> Pred ! Next end, SendTupels),
     H ! {start_election, self()},
+    timer:sleep (1000),
+    C ! {c_print_to_file, "single.msc"},
+    C ! {c_clear_cache},
+
+    [_,_,H2|_] = T,
+    H ! {start_election, self()},
+    H2 ! {start_election, self()},
+    timer:sleep (1000),
+    C ! {c_print_to_file, "double.msc"},
     ok.
