e161c9277b6ad2371fc622c6677caedf0f52d450
Kai Warncke <warncke@informatik.hu-berlin.de>
2011-02-01 18:36:20 +0100

    Acceptor first simple cases ...

--- src/a_11/acceptor.erl
+++ src/a_11/acceptor.erl
@@ -6,7 +6,26 @@
 % Initialize an acceptor
 %
 
-initialize (Collector) ->
-    true
-    . %% END OF FUNCTION
+initialize(C) -> 
+	MyPid = Pid,
+	R_ack = 0,
+	R_acc = 0,
+	V = null.
+
+receive 
+	{{prep,R_rcv},Sender}  -> 
+		if R_rcv > R_ack, R_rcv > R_acc -> 
+			R_ack = R_rcv,
+			Sender ! {{Sender,{ack,R_ack,V,R_acc}},self()},
+		end. 
+			
+	{{acced,R,W}, Sender} ->
+		if R >= R_ack, R > R_acc -> 
+			R_acc = R,
+			V = W,
+			[X ! {{acced,R_acc,V},self()}|| X <- LearnerList],
+		end.	
+			
+end.
+
 
