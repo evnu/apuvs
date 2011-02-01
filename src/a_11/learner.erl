58761609fd86257f2445af51d06bc7dcbe74cd5f
Jan <jangingnicht@web.de>
2011-02-01 21:54:19 +0100

    merge

--- src/a_11/learner.erl
+++ src/a_11/learner.erl
@@ -6,7 +6,17 @@
 % Initialize a learner
 %
 
-initialize (Collector) ->
-    true
-
+initialize (Collector, Maj) ->
+    learner(Collector, Maj, 1, 0)
     . %% END OF FUNCTION
+
+learner(Collector, Maj, R , Num) ->
+    if
+        Num >= Maj ->
+            decided(
+    receive
+        {R, V, _} ->
+            learner(Collector, Maj, R, Num + 1);
+        {Rother, _, _} ->
+            learner(Collector, Maj, Rother, 0)
+    end.
