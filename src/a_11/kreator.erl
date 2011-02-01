0bf419e2223fe4d247dfae33069e7de5a250c46d
Magnus Mueller <mamuelle@informatik.hu-berlin.de>
2011-02-01 22:11:12 +0100

    calculate majority in creator

--- src/a_11/kreator.erl
+++ src/a_11/kreator.erl
@@ -7,12 +7,14 @@ start(N) ->
 create (N, F, L) when is_integer(N) and is_integer(F) and is_integer(L) ->
     % Initialize a collector to build the msc
     C = collector:start_collector(),
+    % Calculate majority
+    Majority = N / 2 + 1,
     % create N proposers
     Proposers = [spawn (proposer, initialize, [C]) || _ <- lists:seq(1,N)],
     % create F acceptors
     Acceptors = [spawn (acceptor, initialize, [C]) || _ <- lists:seq(1,F)],
     % create L learners
-    Learners  = [spawn (learner , initialize, [C]) || _ <- lists:seq(1,L)],
+    Learners  = [spawn (learner , initialize, [C, Majority]) || _ <- lists:seq(1,L)],
 
     [Acc ! {Learners, self()} || Acc <- Acceptors],
     [Prop ! {Acceptors, self()} || Prop <- Proposers]
